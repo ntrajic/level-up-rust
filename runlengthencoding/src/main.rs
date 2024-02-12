1fbbb20cd530e066b82b13a327301939c60d8354
ntrajic <ntrajic@acm.org>
2024-02-11 16:40:40 -0800

    encode/decode, rust impl for rle

--- runlengthencoding/src/main.rs
+++ runlengthencoding/src/main.rs
@@ -53,20 +53,52 @@
 //     print("rle_encode('aaaabcccaa') is '4a1b3c2a' ", rle_encode(orig) )      # rle_encode('aaaabcccaa') is '4a1b3c2a'
 //     print("rle_decode('4a1b3c2a') is 'aaaabcccaa' ", rle_decode(encoded) )   # rle_decode('4a1b3c2a') is 'aaaabcccaa'
 
-/// Rust RLE implementation - Beginning
+/// Rust RLE implementation - End of impl.
 
 mod run_length_encoding {
     pub fn encode(text: &str) -> String {
-        todo!()
+        let mut count = 0;
+        let mut prev: Option<char> = None;
+        let mut encoded = String::with_capacity(text.len() / 2);
+        let mut chars = text.chars();
+
+        while let Some(c) = chars.next() {
+            if prev.is_none() {
+                prev = Some(c);
+            }
+
+            if prev.unwrap() != c || count == 9 {
+                encoded.push_str(&format!("{}{}", count, prev.unwrap()));
+                count = 0
+            }
+            prev = Some(c);
+            count += 1;
+        }
+
+        // protect against empty string
+        if let Some(prev) = prev {
+            encoded.push_str(&format!("{}{}", count, prev));
+        }
+        encoded
     }
-    
+
     pub fn decode(text: &str) -> String {
-        todo!()
+        let mut decoded = String::with_capacity(text.len() * 2);
+        let mut chars = text.chars();
+
+        while let (Some(n), Some(c)) = (chars.next(), chars.next()) {
+            let n = n.to_digit(10).unwrap();
+            for _ in 0..n {
+                decoded.push(c);
+            }
+        }
+
+        decoded
     }
 }
 
 fn main() {
-    // 
+    //
 }
 
 #[test]
@@ -81,7 +113,6 @@ fn round_trip() {
     use run_length_encoding::*;
 
     let input = "LinkedIn";
-    println!("{}", encode(input));
     assert_eq!(decode(&encode(input)), input);
 }
 
@@ -92,3 +123,10 @@ fn long_run() {
     let input = "AAAAA AAAAAAAAAA AAAAAAAAAAAAAAAAAAAA";
     assert_eq!(encode(input), "5A1 9A1A1 9A9A2A");
 }
+
+// ntrajic@DESKTOP-6PK7L32:/mnt/c/SRC/Rust/level-up-rust/runlengthencoding$ make test
+// cargo test --quiet
+//
+// running 3 tests
+// ...
+// test result: ok. 3 passed; 0 failed; 0 ignored; 0 measured; 0 filtered out; finished in 0.00s
