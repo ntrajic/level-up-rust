b57a4a01d4a067b766bc4187387098f8899a6fef
Nikola D Trajic <ntrajic@users.noreply.github.com>
2024-02-19 07:44:15 +0000

    implemented by using closure

--- missingsumfactor/src/main.rs
+++ missingsumfactor/src/main.rs
@@ -6,15 +6,22 @@
 //}
 // e.g 1                                          e.g.2:
 // let data = vec![Some(4), None, Some(1), ];     let data = vec![None, None, None,];   
-// let answer = sum_with_missing(data);           let anslwer = sum_with_missing(data);
+// let answer = sum_with_missing(data);           let answer = sum_with_missing(data);
 // assert_eq!(answer, 5);                         assert_eq!(answer, 0)
 
 fn sum_with_missing(numbers: Vec<Option<i32>>) -> i32 {
-    todo!();
+    //todo!();
+    numbers
+        .iter()
+        .map(|x| x.unwrap_or_default())   // default() is zero, unwrap x: Option<i32>, where |x| x.unwrap_or_default() is  CLOSURE,
+        .sum()                            // assumes closeness to pass elems of numbers of type Option<32> to function
 }
 
 fn main() {
     println!("");
+    let data = vec![Some(3),Some(4),None,Some(5)];
+    let answer: i32 = sum_with_missing(data);
+    println!("{}", answer);
 }
 
 
@@ -42,8 +49,27 @@ fn all_missing() {
     assert_eq!(sum_with_missing(nn), 0);
 }
 
+// with an iterative implementaion of summation
+// $ make test
+// cargo test --quiet
+//
+// running 4 tests
+// ....
+// test result: ok. 4 passed; 0 failed; 0 ignored; 0 measured; 0 filtered out; finished in 0.00s
 
-
+// $ make run
+// cargo run                               # won't work, cargo run -- play --name "Marco"   # where "Marco" is 2nd arg after play, supplided ater --name
+//    Compiling missingsumfactorS v0.1.0 (/workspaces/level-up-rust/missingsumfactor)
+// warning: crate `missingsumfactorS` should have a snake case name
+//   |
+//   = help: convert the identifier to snake case: `missingsumfactor_s`
+//   = note: `#[warn(non_snake_case)]` on by default
+//
+// warning: `missingsumfactorS` (bin "missingsumfactorS") generated 1 warning
+//     Finished dev [unoptimized + debuginfo] target(s) in 0.22s
+//      Running `target/debug/missingsumfactorS`
+//
+// 12
 
 // $ cargo test
 //    Compiling removedups v0.1.0 (/workspaces/level-up-rust/missingsumfactor)
