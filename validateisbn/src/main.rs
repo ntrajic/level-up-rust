13c5cc9255dd4de21860264c7d81580ed7bcc082
Nikola D Trajic <ntrajic@users.noreply.github.com>
2024-02-20 00:24:49 +0000

    isbn string sequence scanned and relationships & constraints between digits checked

--- validateisbn/src/main.rs
+++ validateisbn/src/main.rs
@@ -5,16 +5,58 @@
 
 use std::str::FromStr;
 
+
 struct Isbn {
     raw: String,
     digits: Vec<u8>,
 }
 
+impl Isbn {
+    fn new(raw: String, digits: Vec<u8>) -> Self {
+        Self { raw, digits }
+    }
+}
+
+#[derive(Debug)]
+enum InvalidIsbn {                          // describe errors enumInvalidIsbn structure
+    TooLong,                                // len > 13
+    TooShort,                               // len < 13
+    InvalidCharacter(usize, char),          //  InvalidCharacter(usize, char);
+    FailedChecksum,                         // for each 12 digits, calculate checksum
+}
+
 impl FromStr for Isbn {
-    type Err = (); // TODO: replace with appropriate type
+    type Err = InvalidIsbn;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        todo!();        
+        let mut digits = vec![];
+
+        for (i, c) in s.char_indices() {            // iterate thru enumerated character tuples (idx, char) returned by string.char_indices()                  
+            match c {                                           // match on two choices: 0. sign (negative) 1. true digit 2. InvalidIsbn struct, after unpacking/unwrapping the Option    
+                '-' => continue,
+                '0'..='9' => digits.push(c.to_digit(10).unwrap() as u8),
+                _ => {
+                    return Err(InvalidIsbn::InvalidCharacter(i, c));
+                }
+            }
+        }
+        match digits.len() {
+            len if len < 13 => return Err(InvalidIsbn::TooShort),
+            len if len > 13 => return Err(InvalidIsbn::TooLong),
+            _ => (),
+        }
+        
+        match digits.get(12) {
+            Some(&digit) if digit != calculate_check_digit(&digits) => {
+                return Err(InvalidIsbn::FailedChecksum);
+            }
+            _ => (),
+        }
+        
+        Ok(Isbn {
+            digits,
+            raw: s.to_string(),
+        })
     }
 }
 
@@ -26,13 +68,29 @@ impl std::fmt::Display for Isbn {
 
 // https://en.wikipedia.org/wiki/International_Standard_Book_Number#ISBN-13_check_digit_calculation
 fn calculate_check_digit(digits: &[u8]) -> u8 {
-    todo!()
+    const WEIGHTS: [u8; 12] = [1, 3, 1, 3, 1, 3, 1, 3, 1, 3, 1, 3];
+
+    let weights_applied: u32 = digits
+        .iter()
+        .zip(WEIGHTS.iter())
+        .map(|(&x, &y)| x * y)
+        .map(|x| x as u32)
+        .sum();
+
+    let check_digit = 10 - (weights_applied % 10);
+
+    match check_digit {
+        10 => 0_u8,
+        m => m as u8,
+    }
 }
 
 fn main() {
-    let rust_in_action: Isbn = "978-3-16-148410-0".parse().unwrap();
+    let rust_in_action: Isbn = "978-3-16-148410-0"
+        .parse()
+        .unwrap();
 
-    println!("Rust in Action's ISBN-13 ({})is valid!", rust_in_action);
+    println!("Rust in Action's ISBN-13 ({}) is valid!", rust_in_action);
 }
 
 #[test]
@@ -53,6 +111,12 @@ fn can_correctly_calculate_check_digits() {
 fn rust_in_action() {
     let _: Isbn = "978-3-16-148410-0".parse().unwrap();
 }
+
+// (bin "validateisbn") 
+//     Finished dev [unoptimized + debuginfo] target(s) in 0.00s
+//      Running `target/debug/validateisbn`
+// Rust in Action's ISBN-13 (978-3-16-148410-0) is valid!
+//  *  Terminal will be reused by tasks, press any key to close it. 
 
 
 // $ make test
