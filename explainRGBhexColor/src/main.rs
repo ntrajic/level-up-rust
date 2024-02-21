1f0fed1dcefc16c112f650be80be3006b3d2c031
Nikola D Trajic <ntrajic@users.noreply.github.com>
2024-02-21 01:04:37 +0000

    filled in implmentations.

--- explainRGBhexColor/src/main.rs
+++ explainRGBhexColor/src/main.rs
@@ -2,7 +2,19 @@ use std::fmt::Display;
 use std::str::FromStr;
 
 #[derive(Debug, PartialEq)]
-struct Rgb; // TODO: design data structure
+enum ParseColorError {
+    RedOutOfBounds,
+    BlueOutOfBounds,
+    GreenOutOfBounds,
+    Invalid,
+}
+
+#[derive(Debug, PartialEq)]
+struct Rgb {
+    r: u8,
+    g: u8,
+    b: u8,
+}
 
 trait RgbChannels {
     fn r(&self) -> u8;
@@ -13,11 +25,37 @@ trait RgbChannels {
 }
 
 impl RgbChannels for Rgb {
-    // TODO: implement trait
+    fn r(&self) -> u8 {
+        self.r
+    }
+
+    fn g(&self) -> u8 {
+        self.g
+    }
+
+    fn b(&self) -> u8 {
+        self.b
+    }
 }
 
 impl FromStr for Rgb {
-    // TODO: implement trait
+    type Err = ParseColorError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if let Some(hex) = s.strip_prefix('#') {
+            let r = u8::from_str_radix(&hex[0..2], 16)
+                .or_else(|_| Err(ParseColorError::RedOutOfBounds))?;
+            let g = u8::from_str_radix(&hex[2..4], 16)
+                .or_else(|_| Err(ParseColorError::BlueOutOfBounds))?;
+            let b = u8::from_str_radix(&hex[4..6], 16)
+                .or_else(|_| Err(ParseColorError::GreenOutOfBounds))?;
+
+            Ok(Rgb { r, g, b })
+        } else {
+            // no leading #
+            Err(ParseColorError::Invalid)
+        }
+    }
 }
 
 impl Display for Rgb {
